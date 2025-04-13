f2e0350ea7c132b5b2c3bc60726248086e08173a
Vahid Haeri <visper@gmail.com>
2025-04-12 19:46:52 -0700

    Update document analysis modal title and button text to reflect broader functionality

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -784,7 +784,7 @@ export function TaskList({ initialTasks = [] }: TaskListProps) {
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-purple-500" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4z" clipRule="evenodd" />
                       </svg>
-                      PDF Analysis
+                      Document Analysis
                     </button>
                   </div>
                 </div>
@@ -973,8 +973,8 @@ export function TaskList({ initialTasks = [] }: TaskListProps) {
       {showBulkAddPDFForm && (
         <Modal 
           onClose={() => setShowBulkAddPDFForm(false)} 
-          title="Extract Tasks from PDF" 
-          size="2xl"
+          title="Extract Tasks from Documents" 
+          size="lg"
           preventOutsideClose={true} 
         >
           <BulkAddTaskFromPDF
